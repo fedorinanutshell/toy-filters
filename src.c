c4b9f5840343e46f3b1fa23f89e9077737f05258
fedorinanutshell <>
2026-03-16 10:31:39 +0300

    refactoring

--- src.c
+++ src.c
@@ -7,54 +7,63 @@ struct bilin {
   float x_1, y_1;
 };
 
-struct bilin bilin_lp(float omega) {
+struct bilin bilin(float a_0, float a_1,
+                   float b_0, float b_1) {
   return (struct bilin) {
-    sinf(omega) + 1.f + cosf(omega),
-    sinf(omega) - 1.f - cosf(omega),
-    sinf(omega),
-    sinf(omega),
+    a_0, a_1,
+    b_0, b_1,
     .0f, .0f,
   };
 }
 
-struct bilin bilin_ls(float omega, float a) {
-  return (struct bilin) {
-    1.f / a * sinf(omega) + 1.f + cosf(omega),
-    1.f / a * sinf(omega) - 1.f - cosf(omega),
-    a * sinf(omega) + 1.f + cosf(omega),
-    a * sinf(omega) - 1.f - cosf(omega),
-    .0f, .0f,
-  };
+struct bilin bilin_lp(float o) {
+  const float s = sinf(o), c = cosf(o);
+  return bilin(
+    s + 1.f + c,
+    s - 1.f - c,
+    s,
+    s
+  );
 }
 
-struct bilin bilin_hp(float omega) {
-  return (struct bilin) {
-    sinf(omega) + 1.f + cosf(omega),
-    sinf(omega) - 1.f - cosf(omega),
-    1.f + cosf(omega),
-    -1.f - cosf(omega),
-    .0f, .0f,
-  };
+struct bilin bilin_ls(float o, float g) {
+  const float s = sinf(o), c = cosf(o);
+  return bilin(
+    1.f / g * s + 1.f + c,
+    1.f / g * s - 1.f - c,
+    g * s + 1.f + c,
+    g * s - 1.f - c
+  );
 }
 
-struct bilin bilin_hs(float omega, float a) {
-  return (struct bilin) {
-    sinf(omega) + 1.f / a + 1.f / a * cosf(omega),
-    sinf(omega) - 1.f / a - 1.f / a * cosf(omega),
-    sinf(omega) + a + a * cosf(omega),
-    sinf(omega) - a - a * cosf(omega),
-    .0f, .0f,
-  };
+struct bilin bilin_hp(float o) {
+  const float s = sinf(o), c = cos(o);
+  return bilin(
+    s + 1.f + c,
+    s - 1.f - c,
+    1.f + c,
+    -1.f - c
+  );
 }
 
-struct bilin bilin_ap(float omega) {
-  return (struct bilin) {
-    sinf(omega) + 1.f + cosf(omega),
-    sinf(omega) - 1.f - cosf(omega),
-    sinf(omega) - 1.f - cosf(omega),
-    sinf(omega) + 1.f + cosf(omega),
-    .0f, .0f,
-  };
+struct bilin bilin_hs(float o, float g) {
+  const float s = sinf(o), c = cosf(o);
+  return bilin(
+    s + 1.f / g + 1.f / g * c,
+    s - 1.f / g - 1.f / g * c,
+    s + g + g * c,
+    s - g - g * c
+  );
+}
+
+struct bilin bilin_ap(float o) {
+  const float s = sinf(o), c = cosf(o);
+  return bilin(
+    s + 1.f + c,
+    s - 1.f - c,
+    s - 1.f - c,
+    s + 1.f + c
+  );
 }
 
 void bilin_proc(const float *x, float *y, size_t n, struct bilin *bilin) {
@@ -69,108 +78,109 @@ struct biquad {
   float x_1, x_2, y_1, y_2;
 };
 
-struct biquad biquad_peq(float omega, float q, float a) {
-  float alpha = sinf(omega) / (2.f * q);
+struct biquad biquad(float a_0, float a_1, float a_2,
+                     float b_0, float b_1, float b_2) {
   return (struct biquad) {
-    1.f + alpha / a,
-    -2.f * cosf(omega),
-    1.f - alpha / a,
-    1.f + alpha * a,
-    -2.f * cosf(omega),
-    1.f - alpha * a,
+    a_0, a_1, a_2,
+    b_0, b_1, b_2,
     .0f, .0f, .0f, .0f,
   };
 }
 
-struct biquad biquad_lp(float omega, float q) {
-  float alpha = sinf(omega) / (2.f * q);
-  return (struct biquad) {
-    1.f + alpha,
-    -2.f * cosf(omega),
-    1.f - alpha,
-    (1.f - cosf(omega)) / 2.f,
-    1.f - cosf(omega),
-    (1.f - cosf(omega)) / 2.f,
-    .0f, .0f, .0f, .0f,
-  };
+struct biquad biquad_peq(float o, float q, float g) {
+  const float s = sinf(o), c = cosf(o), a = s / (2.f * q);
+  return biquad(
+    1.f + a / g,
+    -2.f * c,
+    1.f - a / g,
+    1.f + a * g,
+    -2.f * c,
+    1.f - a * g
+  );
 }
 
-struct biquad biquad_ls(float omega, float q, float a) {
-  float alpha = sinf(omega) / (2.f * q);
-  return (struct biquad) {
-    (a + 1.f) + (a - 1.f) * cosf(omega) + 2.f * sqrtf(a) * alpha,
-    -2.f * ((a - 1.f) + (a + 1.f) * cosf(omega)),
-    (a + 1.f) + (a - 1.f) * cosf(omega) - 2.f * sqrtf(a) * alpha,
-    a * ((a + 1.f) - (a - 1.f) * cosf(omega) + 2.f * sqrtf(a) * alpha),
-    2.f * a * ((a - 1.f) - (a + 1.f) * cosf(omega)),
-    (a + 1.f) + (a - 1.f) * cosf(omega) - 2 * sqrtf(a) * alpha,
-    .0f, .0f, .0f, .0f,
-  };
+struct biquad biquad_lp(float o, float q) {
+  const float s = sinf(o), c = cosf(o), a = s / (2.f * q);
+  return biquad(
+    1.f + a,
+    -2.f * c,
+    1.f - a,
+    (1.f - c) / 2.f,
+    1.f - c,
+    (1.f - c) / 2.f
+  );
 }
 
-struct biquad biquad_hp(float omega, float q) {
-  float alpha = sinf(omega) / (2.f * q);
-  return (struct biquad) {
-    1.f + alpha,
-    -2.f * cosf(omega),
-    1.f - alpha,
-    (1.f + cosf(omega)) / 2.f,
-    -(1.f + cosf(omega)),
-    (1.f + cosf(omega)) / 2.f,
-    .0f, .0f, .0f, .0f,
-  };
+struct biquad biquad_ls(float o, float q, float g) {
+  const float s = sinf(o), c = cosf(o), a = s / (2.f * q), r = sqrtf(g);
+  return biquad(
+    (g + 1.f) + (g - 1.f) * c + 2.f * r * a,
+    -2.f * ((g - 1.f) + (g + 1.f) * c),
+    (g + 1.f) + (g - 1.f) * c - 2.f * r * a,
+    g * ((g + 1.f) - (g - 1.f) * c + 2.f * r * a),
+    2.f * g * ((g - 1.f) - (g + 1.f) * c),
+    (g + 1.f) + (g - 1.f) * c - 2 * r * a
+  );
 }
 
-struct biquad biquad_hs(float omega, float q, float a) {
-  float alpha = sinf(omega) / (2.f * q);
-  return (struct biquad) {
-    (a + 1.f) + (a - 1.f) * cosf(omega) + 2.f * sqrtf(a) * alpha,
-    2.f * ((a - 1.f) + (a + 1.f) * cosf(omega)),
-    (a + 1.f) + (a - 1.f) * cosf(omega) - 2.f * sqrtf(a) * alpha,
-    a * ((a + 1.f) + (a - 1.f) * cosf(omega) + 2.f * sqrtf(a) * alpha),
-    -2.f * a * ((a - 1.f) + (a + 1.f) * cosf(omega)),
-    (a + 1.f) + (a + 1.f) * cosf(omega) - 2 * sqrtf(a) * alpha,
-    .0f, .0f, .0f, .0f,
-  };
+struct biquad biquad_hp(float o, float q) {
+  const float s = sinf(o), c = cosf(o), a = s / (2.f * q);
+  return biquad(
+    1.f + a,
+    -2.f * c,
+    1.f - a,
+    (1.f + c) / 2.f,
+    -(1.f + c),
+    (1.f + c) / 2.f
+  );
 }
 
-struct biquad biquad_bp(float omega, float q) {
-  float alpha = sinf(omega) / (2.f * q);
-  return (struct biquad) {
-    1.f + alpha,
-    -2.f * cosf(omega),
-    1.f - alpha,
-    alpha,
+struct biquad biquad_hs(float o, float q, float g) {
+  const float s = sinf(o), c = cosf(o), a = s / (2.f * q), r = sqrtf(g);
+  return biquad(
+    (g + 1.f) + (g - 1.f) * c + 2.f * r * a,
+    2.f * ((g - 1.f) + (g + 1.f) * c),
+    (g + 1.f) + (g - 1.f) * c - 2.f * r * a,
+    g * ((g + 1.f) + (g - 1.f) * c + 2.f * r * a),
+    -2.f * g * ((g - 1.f) + (g + 1.f) * c),
+    (g + 1.f) + (g + 1.f) * c - 2 * r * a
+  );
+}
+
+struct biquad biquad_bp(float o, float q) {
+  const float s = sinf(o), c = cosf(o), a = s / (2.f * q);
+  return biquad(
+    1.f + a,
+    -2.f * c,
+    1.f - a,
+    a,
     0,
-    -alpha,
-    .0f, .0f, .0f, .0f,
-  };
+    -a
+  );
 }
 
-struct biquad biquad_notch(float omega, float q) {
-  float alpha = sinf(omega) / (2.f * q);
-  return (struct biquad) {
-    1.f + alpha,
-    -2.f * cosf(omega),
-    1.f - alpha,
+struct biquad biquad_notch(float o, float q) {
+  const float s = sinf(o), c = cosf(o), a = s / (2.f * q);
+  return biquad(
+    1.f + a,
+    -2.f * c,
+    1.f - a,
     1.f,
-    -2.f * cosf(omega),
-    1.f,
-    .0f, .0f, .0f, .0f,
-  };
+    -2.f * c,
+    1.f
+  );
 }
 
-struct biquad biquad_ap(float omega, float q) {
-  float alpha = sinf(omega) / (2.f * q);
-  return (struct biquad) {
-    1.f + alpha,
-    -2.f * cosf(omega),
-    1.f - alpha,
-    1.f - alpha,
-    -2.f * cosf(omega),
-    1.f + alpha,
-    .0f, .0f, .0f, .0f,
-  };
+struct biquad biquad_ap(float o, float q) {
+  const float s = sinf(o), c = cosf(o), a = s / (2.f * q);
+  return biquad(
+    1.f + a,
+    -2.f * c,
+    1.f - a,
+    1.f - a,
+    -2.f * c,
+    1.f + a
+  );
 }
 
 void biquad_proc(const float *x, float *y, size_t n, struct biquad *biquad) {
